ca742a72d0fdb32b617c5fdf5ff2493d7ff45878
Shaurya Srivastava <poonam61276@gmail.com>
2026-04-03 20:06:13 +0530

    Tenth commit

---
+++ Day_10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s[1001];
+    if (scanf("%1000s", s) != 1) {
+        return 0;
+    }
+    int left = 0;
+    int right = strlen(s) - 1;
+    int isPalindrome = 1; 
+    while (left < right) {
+        if (s[left] != s[right]) {
+            isPalindrome = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+    if (isPalindrome) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    return 0;
+}
